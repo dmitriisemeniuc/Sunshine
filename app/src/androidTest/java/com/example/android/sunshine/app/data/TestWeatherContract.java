7faa8ff6acdbfe9d7f15c69a8b0b07593cd46805
Dmitrii Semeniuc <dmitriisemeniuc@gmail.com>
2016-07-14 15:44:21 +0300

    Add a Weather w/Location

---
+++ app/src/androidTest/java/com/example/android/sunshine/app/data/TestWeatherContract.java
@@ -0,0 +1,23 @@
+package com.example.android.sunshine.app.data;
+
+import android.net.Uri;
+import android.test.AndroidTestCase;
+
+public class TestWeatherContract extends AndroidTestCase {
+
+    // intentionally includes a slash to make sure Uri is getting quoted correctly
+    private static final String TEST_WEATHER_LOCATION = "/North Pole";
+    private static final long TEST_WEATHER_DATE = 1419033600L;  // December 20th, 2014
+
+    public void testBuildWeatherLocation() {
+        Uri locationUri = WeatherContract.WeatherEntry.buildWeatherLocation(TEST_WEATHER_LOCATION);
+        assertNotNull("Error: Null Uri returned.  You must fill-in buildWeatherLocation in " +
+                        "WeatherContract.",
+                locationUri);
+        assertEquals("Error: Weather location not properly appended to the end of the Uri",
+                TEST_WEATHER_LOCATION, locationUri.getLastPathSegment());
+        assertEquals("Error: Weather location Uri doesn't match our expected result",
+                locationUri.toString(),
+                "content://com.example.android.sunshine.app/weather/%2FNorth%20Pole");
+    }
+}
